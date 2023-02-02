0872582002e2b143bc9641775dd74d99f9de4ce1
Dawson Timmons <DawsonTimmons@gmail.com>
2023-02-01 18:49:38 -0700

    Add passing test for displaying parent on show page

--- spec/features/players/show_spec.rb
+++ spec/features/players/show_spec.rb
@@ -16,4 +16,14 @@ RSpec.describe 'the players show page' do
     expect(page).to_not have_content(player_2.height_in_cm)
     expect(page).to_not have_content(player_2.position)
   end
+
+  it "displays the team the player belongs to" do
+    team = Team.create!( name: 'Nuggets', city: 'Denver', rank: 1, confrence: "west", has_won_championship: false )
+    player_1 = team.players.create!( name: 'Nikola Jokić', age: 27, height_in_cm: 210, position: "center", is_a_rookie: false)
+    player_2 = team.players.create!( name: 'Jamal Murray', age: 25, height_in_cm: 193, position: "point guard", is_a_rookie: false)
+    visit "/players/#{player_1.id}" 
+
+    expect(page).to have_content(team.name)
+  end
+
 end
