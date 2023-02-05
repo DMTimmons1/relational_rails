71f759d2e090f102120bd83728d28b67f5261941
Dawson Timmons <DawsonTimmons@gmail.com>
2023-02-05 16:19:20 -0700

    Add edit functionallity to teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -12,8 +12,22 @@ class TeamsController < ApplicationController
   end
 
   def create
-    team = Team.create(name: params[:name])
+    team = Team.create(team_params)
     redirect_to "/teams"
   end
 
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    team = Team.find(params[:id])
+    team.update(team_params)
+    redirect_to '/teams'
+  end
+  
+private
+  def team_params
+    params.permit(:name, :city, :conference, :rank, :has_won_championship)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get '/players', to: 'players#index'
   get '/players/:id', to: 'players#show'
   post '/teams', to: 'teams#create'
+  get '/teams/:id/edit', to: 'teams#edit'
+  patch 'teams/:id', to: 'teams#update'
 end
